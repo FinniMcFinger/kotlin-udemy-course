b8e961818cce0b8ac53d0e603e8c5bfaa22177f5
Randall McGrew <rrmcgrew@gmail.com>
2026-03-10 14:56:45 -0500

    explores val and var

---
+++ src/main/kotlin/basics/Variables.kt
@@ -0,0 +1,15 @@
+package com.finnimcfinger.basics
+
+fun main() {
+    val name : String = "FinniMcFinger"
+    println(name)
+
+    // val is immutable
+//    name = "Finni McFinger"
+
+    var age : Int = 44
+    println(age)
+    // var is mutable
+    age = 45
+    println(age)
+}

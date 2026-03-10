6edb063e6083718acd51858e501120867ac5a9df
Randall McGrew <rrmcgrew@gmail.com>
2026-03-10 15:27:51 -0500

    implements while and do-while loops

--- src/main/kotlin/basics/LoopsAndRanges.kt
+++ src/main/kotlin/basics/LoopsAndRanges.kt
@@ -19,4 +19,23 @@ fun main() {
     for (i in reverse step 2) {
         println("reverse value with skip: $i")
     }
+
+    exploreWhile()
+    exploreDoWhile()
+}
+
+fun exploreWhile() {
+    var x = 1;
+
+    while (x < 5) {
+        println("value of x is ${x++}")
+    }
+}
+
+fun exploreDoWhile() {
+    var y = 0
+
+    do {
+        println("value of y is ${y++}")
+    } while (y < 5)
 }

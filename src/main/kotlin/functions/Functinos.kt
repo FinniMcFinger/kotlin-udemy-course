4387a14937d888f85d22b593378c8d7237c409da
Randall McGrew <rrmcgrew@gmail.com>
2026-03-10 16:15:49 -0500

    illustrates named parameters and default values

--- src/main/kotlin/functions/Functinos.kt
+++ src/main/kotlin/functions/Functinos.kt
@@ -1,5 +1,7 @@
 package com.finnimcfinger.functions
 
+import java.time.LocalDate
+
 // Unit serves as the void type does in Java; declaring it is redundant as it is inferred
 fun printName(name: String) {
     println("Name: $name")
@@ -13,10 +15,26 @@ fun addition(i: Int, j: Int): Int {
 // another way to declare functions, as statements; type inferred
 fun additionCompact(i: Int, j: Int) = i + j
 
+fun printPersonDetails(
+    name: String, // no default, required
+    email: String = "",
+    dateOfBirth: LocalDate = LocalDate.now()
+) {
+    println("Name: $name, Email: $email, Date of Birth: $dateOfBirth")
+}
+
 fun main() {
     // passing a param
     val unit = printName("Finni McFinger")
     println("unit is of type: $unit")
     println("calling addition of 3 and 4: ${addition(3, 4)}")
     println("calling compact addition of 3 and 4: ${additionCompact(3, 4)}")
+    printPersonDetails("Finni McFinger", "example@example.com", LocalDate.parse("1981-10-07"))
+    printPersonDetails("Finni McFinger")
+    printPersonDetails(
+        email = "example@example.com",
+        name = "Finni McFinger",
+        dateOfBirth = LocalDate.parse("1981-10-07")
+    )
+
 }

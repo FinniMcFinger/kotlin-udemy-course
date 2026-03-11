e75a6c9e2def626a8f832d7118680945e13a5065
Randall McGrew <rrmcgrew@gmail.com>
2026-03-11 10:19:42 -0500

    illustrates inheritance

---
+++ src/main/kotlin/classes/User.kt
@@ -0,0 +1,40 @@
+package com.finnimcfinger.classes
+
+// `open` un-finalizes the class and allows it to be extended
+open class User(val name: String) {
+    var isLoggedIn = false
+    open var title = "User"
+
+    open fun login() {
+        println("User base class login")
+        this.isLoggedIn = true
+    }
+}
+
+//                           ↓ extends User
+class Student(name: String): User(name) {
+    // overriding the `title` field from `User`
+    override var title = "Student"
+
+    // overriding the login function from `User`
+    override fun login() {
+        println("Student class login")
+        super.login()
+    }
+}
+
+class Instructor(name: String): User(name) {
+    override var title = "Instructor"
+}
+
+fun main() {
+    val user = User("Nobody")
+    user.login()
+    println("${user.title} ${user.name} logged in? ${user.isLoggedIn}")
+    val student = Student("Marty")
+    student.login()
+    println("${student.title} ${student.name} logged in? ${student.isLoggedIn}")
+    val instructor = Instructor("Doc")
+    instructor.login()
+    println("${instructor.title} ${instructor.name} logged in? ${instructor.isLoggedIn}")
+}

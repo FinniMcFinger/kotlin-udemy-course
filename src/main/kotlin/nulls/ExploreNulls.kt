1778331f5ba7786110498236bb2342189725adc3
Randall McGrew <rrmcgrew@gmail.com>
2026-03-11 14:35:54 -0500

    demonstrates more null handling

--- src/main/kotlin/nulls/ExploreNulls.kt
+++ src/main/kotlin/nulls/ExploreNulls.kt
@@ -8,6 +8,9 @@ data class Movie(
 fun main() {
     var nullable: String? = null
     println("nullable value is $nullable")
+    // nullable type builds compile error without non-null assertion (!!)
+//    printName(nullable!!) // causes NPE
+    nullable?.run { printName(this) } // null-safe lambda to run with nullable value, scope function
     // ?  - null-safe operator
     // ?: - Elvis operator, provides default
     val length = nullable?.length ?: 0
@@ -18,6 +21,8 @@ fun main() {
     println("non-nullable value is $nonNullable")
     // compilation error
 //    nonNullable = null
+    // totally fine
+    printName(nonNullable)
     val movie = Movie(null, "Back to the Future")
     println("movie is $movie")
     saveMovie(movie)
@@ -27,5 +32,13 @@ fun main() {
 }
 
 fun saveMovie(movie: Movie) {
-    movie.id = null
+    movie.id = 1
+}
+
+fun printName(name: String) {
+    println("Name: $name")
+}
+
+fun printNameNullSafe(name: String?) {
+    println("Name: $name")
 }

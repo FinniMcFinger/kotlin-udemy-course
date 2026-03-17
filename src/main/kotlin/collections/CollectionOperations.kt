ad3c91160698135d3f60630d7e635862907f6ea8
FinniMcFinger <rrmcgrew@gmail.com>
2026-03-17 12:39:08 -0500

    explores flatmap operations

--- src/main/kotlin/collections/CollectionOperations.kt
+++ src/main/kotlin/collections/CollectionOperations.kt
@@ -2,14 +2,15 @@ package com.finnimcfinger.collections
 
 import com.finnimcfinger.dataset.Course
 import com.finnimcfinger.dataset.CourseCategory
+import com.finnimcfinger.dataset.KAFKA
 import com.finnimcfinger.dataset.courseList
 
 // `it` keyword can be used to access the element in a collections operator
 
 fun main() {
     val courses = courseList()
-    val developmentPredicate = {c: Course -> c.category == CourseCategory.DEVELOPMENT }
-    val designPredicate = {c: Course -> c.category == CourseCategory.DESIGN }
+    val developmentPredicate = { c: Course -> c.category == CourseCategory.DEVELOPMENT }
+    val designPredicate = { c: Course -> c.category == CourseCategory.DESIGN }
     println("Development Courses:")
     exploreFilter(courses, developmentPredicate)
     println("Design Courses:")
@@ -18,6 +19,17 @@ fun main() {
     exploreMap(courses, developmentPredicate)
     println("Design Course Names/Categories:")
     exploreMap(courses, designPredicate)
+
+    val complexList = listOf(listOf(1, 2, 3), listOf(4, 5, 6))
+    val normalResult = complexList.map{ outerList ->
+        outerList.map{ it.toDouble() }
+    }
+    println("normal map result: $normalResult")
+    val flatResult = complexList.flatMap{ outerList ->  outerList.map{ it.toDouble() }}
+    println("flat map result: $flatResult")
+
+    val kafkaCourses = exploreFlatMap(courses, KAFKA)
+    println("kafka courses: $kafkaCourses")
 }
 
 fun exploreFilter(courses: MutableList<Course>, predicate: (Course) -> Boolean) {
@@ -31,4 +43,13 @@ fun exploreMap(courses: MutableList<Course>, predicate: (Course) -> Boolean) {
         .filter(predicate) // passes predicate directly, no need to invoke
         .map { "${it.name} - ${it.category}" } // returns iterable, forEach refers to the result of .map
         .forEach { println(it) }
+}
+
+fun exploreFlatMap(courses: MutableList<Course>, topic: String): List<String> {
+    return courses.flatMap { course ->
+        val courseName = course.name
+        course.topicsCovered.filter {
+            it == topic
+        } .map { courseName }
+    }
 }
